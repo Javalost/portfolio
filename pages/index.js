a1334d3dba90a96572db742a96d612e160221f74
joseb <thompsonjose3@gmail.com>
2023-11-26 21:26:46 -0600

    Image needs reallignment

--- pages/index.js
+++ pages/index.js
@@ -25,97 +25,81 @@ const Home = () => {
             </Box> 
 
             {/* Gradient from this box */}
-            <Box sx={{
-                    width: '100%', 
-                    height: '15rem',
-                    background: 'linear-gradient(to right, #334641, #897A68, #926557, #E1E5E6)',  
-                    display:'flex', 
-                    flexDirection:'column',
+            <Box
+                sx={{
+                    width: '100%',
+                    height: '25rem',
+                    background: 'linear-gradient(to right, #334641, #897A68, #926557, #E1E5E6)',
+                    display: 'flex',
+                    flexDirection: 'column',
                     alignContent: 'flex-end',
                     alignItems: 'center',
-            }}>            
-                <Box sx={{ 
-                    
-                }}>
+                    position:'relative'
+                }}
+            >
+                {/* ::after pseudo-element for white bottom border */}
+                <Box sx={{                    
+                    position: 'absolute',
+                    left: 0,
+                    width: '100%',
+                    bottom:0,
+                    height: '1%', // Adjust the height to control the white overlay size
+                    background:'white',
+                    zIndex: 1, // Ensure it covers the gradient
+                }} 
+                />
+                <Box sx={{ height: '100%', border:'solid', }}>
                     {/* If using Next.js Image component */}
-                    <Image src="/assets/mydalleimage-removebg.png" alt="Descriptive Alt Text" width={400} height={500} /> 
+                    <Image src="/assets/mydalleimage-removebg.png" alt="Descriptive Alt Text" width={200} height={300} />
                 </Box>
-            </Box>
-            <Box sx={{height:'10rem', background:'yellow', display:'flex', }}>
-                <Box sx={{display:'flex', width:'33%', height:'100%', border:'solid'    }}> {/* Thirds */}
-                    <Box sx={{background:'red', width:'50%'}}> {/* Column1 */}
-                        <Box sx={{width: '100%', height:'33%',background:'blue'}}> {/* Row1-3 * Gradient shoud lead into these boxes */} 
-
-                        </Box> 
-                        <Box sx={{background:'green',width:'100%', height:'33%'}}>
-
+                <Box sx={{ height: '30%', display: 'flex', width: '100%',}}>
+                    <Box sx={{ display: 'flex', width: '33%', height: '100%',}}>
+                        {/* Thirds */}
+                        <Box sx={{ width: '50%', }}>
+                            {/* Column1 */}
+                            <Box sx={{ background:'white',width: '100%', height: '33%' }}>{/* Row1-3 * Gradient shoud lead into these boxes */}</Box>
+                            <Box sx={{ background:'white',width: '100%', height: '33%' }}>{/* Row1-3 * Gradient shoud lead into these boxes */}</Box>
+                            <Box sx={{ background:'white',width: '100%', height: '33%' }}>{/* Row1-3 * Gradient shoud lead into these boxes */}</Box>
                         </Box>
-                        <Box sx={{width:'100%', height:'33%'}}>
-
-                        </Box>
-                    </Box>
-                    <Box sx={{}}> {/* Column1 */}
-                        <Box>   {/* Row4-6 Gradient Should lead into here*/}
-
-                        </Box>
-                        <Box sx={{width:'100%', height:'33%'}}>
-
-                        </Box>
-                        <Box sx={{width:'100%', height:'33%'}}>
-
+                        <Box sx={{width:'50%'}}>
+                            {/* Column2 */}
+                            <Box sx={{ width: '100%', height: '33%' }}></Box>
+                            <Box sx={{ width: '100%', height: '33%' }}></Box>
+                            <Box sx={{ width: '100%', height: '33%' }}></Box>
                         </Box>
                     </Box>
-                </Box>
-                <Box sx={{display:'flex', width:'33%', height:'100%', border:'solid'}}> {/* Thirds */}
-                    <Box sx={{background:'red', width:'50%'}}> {/* Column1 */}
-                    <Box sx={{width: '100%', height:'33%',background:'blue'}}> {/* Row1-3 * Gradient shoud lead into these boxes */} 
-
-                        </Box> 
-                        <Box sx={{background:'green',width:'100%', height:'33%'}}>
-
+                    <Box sx={{ display: 'flex', width: '33%', height: '100%' }}>
+                        {/* Thirds */}
+                        <Box sx={{ width: '50%' }}>
+                            {/* Column3 */}
+                            <Box sx={{ width: '100%', height: '33%' }}>{/* Row1-3 * Gradient shoud lead into these boxes */}</Box>
+                            <Box sx={{ width: '100%', height: '33%' }}></Box>
+                            <Box sx={{ width: '100%', height: '33%' }}></Box>
                         </Box>
-                        <Box sx={{width:'100%', height:'33%'}}>
-
+                        <Box sx={{ width: '50%' }}>
+                            {/* Column4 */}
+                            <Box sx={{ width: '100%', height: '33%' }}></Box>
+                            <Box sx={{ width: '100%', height: '33%' }}></Box>
+                            <Box sx={{ width: '100%', height: '33%' }}></Box>
                         </Box>
                     </Box>
-                    <Box sx={{border:'solid'}}> {/* Column2 */}
-                        <Box>   {/* Row4-6 Gradient should lead into here*/}
-
+                    <Box sx={{ display: 'flex', width: '33%', height: '100%' }}>
+                        {/* Thirds */}
+                        <Box sx={{ width: '50%' }}>
+                            {/* Column5 */}
+                            <Box sx={{ width: '100%', height: '33%' }}>{/* Row1-3 * Gradient shoud lead into these boxes */}</Box>
+                            <Box sx={{ width: '100%', height: '33%' }}></Box>
+                            <Box sx={{ width: '100%', height: '33%' }}></Box>
                         </Box>
-                        <Box sx={{width:'100%', height:'33%'}}>
-
-                        </Box>
-                        <Box sx={{width:'100%', height:'33%'}}>
-
+                        <Box>
+                            {/* Column6 */}
+                            <Box sx={{ width: '100%', height: '33%' }}></Box>
+                            <Box sx={{ width: '100%', height: '33%' }}></Box>
+                            <Box sx={{ width: '100%', height: '33%' }}></Box>
                         </Box>
                     </Box>
                 </Box>
-                <Box sx={{display:'flex', width:'33%', height:'100%', border:'solid'}}> {/* Thirds */}
-                <Box sx={{background:'red', width:'50%'}}> {/* Column1 */}
-                <Box sx={{width: '100%', height:'33%',background:'blue'}}> {/* Row1-3 * Gradient shoud lead into these boxes */} 
-
-                        </Box> 
-                        <Box sx={{background:'green',width:'100%', height:'33%'}}>
-
-                        </Box>
-                        <Box sx={{width:'100%', height:'33%'}}>
-
-                        </Box>
-                    </Box>
-                    <Box> {/* Column2 */}
-                        <Box>   {/* Row4-6 Gradient should lead into here */}
-
-                        </Box>
-                        <Box sx={{width:'100%', height:'33%'}}>
-
-                        </Box>
-                        <Box sx={{width:'100%', height:'33%'}}>
-
-                        </Box>
-                    </Box>
-                </Box>
-            </Box>
-
+            </Box> {/* Closing the Box component for the gradient section */}
             <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', padding: '1rem' }}>
                 <Typography variant='h2' sx={{ padding: '1rem' }}>
                     Welcome to my Portfolio!
