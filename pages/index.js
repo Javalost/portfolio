d7e6dc3d6060f7dee625ad2565859cbb09d6480b
joseb <thompsonjose3@gmail.com>
2023-11-26 19:34:02 -0600

    testingOriginal

--- pages/index.js
+++ pages/index.js
@@ -5,140 +5,131 @@ import Image from 'next/image';
 
 const Home = () => {
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                height: '100vh',
-                padding: 0,
-                margin: 0,
-                overflow: 'hidden' // To ensure pseudo-elements do not overflow
-            }}
-        >
-            {/* Top bar with buttons */}
-            <Box
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    gap: '2rem',
-                    height: '5rem'
-                }}
-            >
+        <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh', padding: 0, margin: 0 }}>
+            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '2rem', height: '5rem' }}> 
                 <Button>
-                    <Typography>Link1</Typography>
+                    <Typography>
+                        Link1
+                    </Typography>
                 </Button>
                 <Button>
-                    <Typography>Link2</Typography>
+                    <Typography>
+                        Link2
+                    </Typography>
                 </Button>
                 <Button>
-                    <Typography>Link3</Typography>
+                    <Typography>
+                        Link3
+                    </Typography>
                 </Button>
             </Box> 
 
-            {/* Gradient Box */}
-            <Box
-                sx={{
+            {/* Gradient from this box */}
+            <Box sx={{
                     width: '100%', 
                     height: '15rem',
-                    background: 'linear-gradient(to right, #334641, #897A68, #926557, #E1E5E6)',
-                    position: 'relative'
-                }}
-            >
-                {/* Image positioned on top of the bottom border */}
-                <Box
-                    sx={{
-                        position: 'absolute',
-                        bottom: 0,
-                        left: '50%',
-                        transform: 'translate(-50%, 50%)',
-                        '& > span': { // Next.js Image component uses a 'span' wrapper
-                            display: 'block', // This ensures the span wrapper doesn't collapse
-                        }
-                    }}
-                >
-                    <Image src="/assets/mydalleimage-removebg.png" alt="Descriptive Alt Text" width={400} height={500} layout="fixed" />
-                </Box>
-            </Box>
-
-            {/* Rows and columns with the possibility of gradient overlay */}
-            <Box
-                sx={{
-                    height: '10rem',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'flex-end',
-                    background: 'linear-gradient(to right, #334641, #897A68, #926557, #E1E5E6)',
-                    position: 'relative'
-                }}
-            >
-                {/* Individual rows with varying opacity */}
-                <Box
-                    sx={{
-                        display: 'flex',
-                        height: '33%',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        backgroundColor: 'rgba(255, 255, 255, 0.3)'
-                    }}
-                >
-
-                </Box>
-                <Box
-                    sx={{
-                        display: 'flex',
-                        height: '33%',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        backgroundColor: 'rgba(255, 255, 255, 0.5)'
-                    }}
-                >
-                </Box>
-                <Box
-                    sx={{
-                        display: 'flex',
-                        height: '33%',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        backgroundColor: 'rgba(255, 255, 255, 0.7)'
-                    }}
-                >
-                </Box>
-            </Box>
-
-            {/* Welcome Text */}
-            <Box
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'center',
+                    background: 'linear-gradient(to right, #334641, #897A68, #926557, #E1E5E6)',  
+                    display:'flex', 
+                    flexDirection:'column',
+                    alignContent: 'flex-end',
                     alignItems: 'center',
-                    flexDirection: 'column',
-                    padding: '1rem'
-                }}
-            >
+            }}>            
+                <Box sx={{ 
+                    
+                }}>
+                    {/* If using Next.js Image component */}
+                    <Image src="/assets/mydalleimage-removebg.png" alt="Descriptive Alt Text" width={400} height={500} /> 
+                </Box>
+            </Box>
+            <Box sx={{height:'10rem', background:'yellow', display:'flex', }}>
+                <Box sx={{display:'flex', width:'33%', height:'100%', border:'solid'    }}> {/* Thirds */}
+                    <Box sx={{background:'red', width:'50%'}}> {/* Column1 */}
+                        <Box sx={{width: '100%', height:'33%',background:'blue'}}> {/* Row1-3 * Gradient shoud lead into these boxes */} 
+
+                        </Box> 
+                        <Box sx={{background:'green',width:'100%', height:'33%'}}>
+
+                        </Box>
+                        <Box sx={{width:'100%', height:'33%'}}>
+
+                        </Box>
+                    </Box>
+                    <Box sx={{}}> {/* Column1 */}
+                        <Box>   {/* Row4-6 Gradient Should lead into here*/}
+
+                        </Box>
+                        <Box sx={{width:'100%', height:'33%'}}>
+
+                        </Box>
+                        <Box sx={{width:'100%', height:'33%'}}>
+
+                        </Box>
+                    </Box>
+                </Box>
+                <Box sx={{display:'flex', width:'33%', height:'100%', border:'solid'}}> {/* Thirds */}
+                    <Box sx={{background:'red', width:'50%'}}> {/* Column1 */}
+                    <Box sx={{width: '100%', height:'33%',background:'blue'}}> {/* Row1-3 * Gradient shoud lead into these boxes */} 
+
+                        </Box> 
+                        <Box sx={{background:'green',width:'100%', height:'33%'}}>
+
+                        </Box>
+                        <Box sx={{width:'100%', height:'33%'}}>
+
+                        </Box>
+                    </Box>
+                    <Box sx={{border:'solid'}}> {/* Column2 */}
+                        <Box>   {/* Row4-6 Gradient should lead into here*/}
+
+                        </Box>
+                        <Box sx={{width:'100%', height:'33%'}}>
+
+                        </Box>
+                        <Box sx={{width:'100%', height:'33%'}}>
+
+                        </Box>
+                    </Box>
+                </Box>
+                <Box sx={{display:'flex', width:'33%', height:'100%', border:'solid'}}> {/* Thirds */}
+                <Box sx={{background:'red', width:'50%'}}> {/* Column1 */}
+                <Box sx={{width: '100%', height:'33%',background:'blue'}}> {/* Row1-3 * Gradient shoud lead into these boxes */} 
+
+                        </Box> 
+                        <Box sx={{background:'green',width:'100%', height:'33%'}}>
+
+                        </Box>
+                        <Box sx={{width:'100%', height:'33%'}}>
+
+                        </Box>
+                    </Box>
+                    <Box> {/* Column2 */}
+                        <Box>   {/* Row4-6 Gradient should lead into here */}
+
+                        </Box>
+                        <Box sx={{width:'100%', height:'33%'}}>
+
+                        </Box>
+                        <Box sx={{width:'100%', height:'33%'}}>
+
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+
+            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', padding: '1rem' }}>
                 <Typography variant='h2' sx={{ padding: '1rem' }}>
                     Welcome to my Portfolio!
                 </Typography> 
 
                 <Typography variant='h9'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 </Typography>
                 <Typography variant='h9'>
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 </Typography>
             </Box>
 
-            {/* Bottom buttons */}
-            <Box
-                sx={{
-                    flexGrow: 1,
-                    width: '100%',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    gap: '2rem'
-                }}
-            >
+            <Box sx={{ flexGrow: 1, width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', gap:'2rem' }}>
                 <LinkButton /> 
                 <LinkButton /> 
                 <LinkButton />
