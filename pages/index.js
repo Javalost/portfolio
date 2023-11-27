5b9d3667edd6c2a66c61658a5416f5d3a3f1ebb4
joseb <thompsonjose3@gmail.com>
2023-11-27 10:16:20 -0600

    Working on sectioned idea

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ const Home = () => {
             <Box
                 sx={{
                     width: '100%',
-                    height: '25rem',
+                    height: '15rem',
                     background: 'linear-gradient(to right, #334641, #897A68, #926557, #E1E5E6)',
                     display: 'flex',
                     flexDirection: 'column',
@@ -38,28 +38,19 @@ const Home = () => {
                 }}
             >
                 {/* ::after pseudo-element for white bottom border */}
-                <Box sx={{                    
-                    position: 'absolute',
-                    left: 0,
-                    width: '100%',
-                    bottom:0,
-                    height: '1%', // Adjust the height to control the white overlay size
-                    background:'white',
-                    zIndex: 1, // Ensure it covers the gradient
-                }} 
-                />
-                <Box sx={{ height: '100%', border:'solid', }}>
+                <Box sx={{ height: '100%', }}>
                     {/* If using Next.js Image component */}
-                    <Image src="/assets/mydalleimage-removebg.png" alt="Descriptive Alt Text" width={200} height={300} />
+                    <Image src="/assets/mydalleimage-removebg.png" alt="Descriptive Alt Text" width={300} height={400} />
                 </Box>
-                <Box sx={{ height: '30%', display: 'flex', width: '100%',}}>
+            </Box> {/* Closing the Box component for the gradient section */} 
+            <Box sx={{ height: '5rem', display: 'flex', width: '100%',background: 'linear-gradient(to right, #334641, #897A68, #926557, #E1E5E6)',}}>
                     <Box sx={{ display: 'flex', width: '33%', height: '100%',}}>
                         {/* Thirds */}
                         <Box sx={{ width: '50%', }}>
                             {/* Column1 */}
-                            <Box sx={{ background:'white',width: '100%', height: '33%' }}>{/* Row1-3 * Gradient shoud lead into these boxes */}</Box>
-                            <Box sx={{ background:'white',width: '100%', height: '33%' }}>{/* Row1-3 * Gradient shoud lead into these boxes */}</Box>
-                            <Box sx={{ background:'white',width: '100%', height: '33%' }}>{/* Row1-3 * Gradient shoud lead into these boxes */}</Box>
+                            <Box sx={{background:'white', width: '100%', height: '33%' }}>{/* Row1-3 * Gradient shoud lead into these boxes */}</Box>
+                            <Box sx={{background:'white', width: '100%', height: '33%' }}>{/* Row1-3 * Gradient shoud lead into these boxes */}</Box>
+                            <Box sx={{background:'white',width: '100%', height: '33%' }}>{/* Row1-3 * Gradient shoud lead into these boxes */}</Box>
                         </Box>
                         <Box sx={{width:'50%'}}>
                             {/* Column2 */}
@@ -97,9 +88,18 @@ const Home = () => {
                             <Box sx={{ width: '100%', height: '33%' }}></Box>
                             <Box sx={{ width: '100%', height: '33%' }}></Box>
                         </Box>
-                    </Box>
-                </Box>
-            </Box> {/* Closing the Box component for the gradient section */}
+                    </Box> 
+                </Box> 
+                <Box sx={{
+                    position: 'absolute',
+                    bottom: '53%',
+                    left: 0,
+                    width: '100%',
+                    height: '5%', // Adjust the height as needed
+                    bgcolor: 'white',
+                    zIndex: 1,
+                }} />
+                
             <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', padding: '1rem' }}>
                 <Typography variant='h2' sx={{ padding: '1rem' }}>
                     Welcome to my Portfolio!
